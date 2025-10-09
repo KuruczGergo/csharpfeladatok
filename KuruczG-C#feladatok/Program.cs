47d9c30e682868aa2d02f26cf584a5b5e5856a8b
Kurucz Gergő <KuruczG@KKSZKI.LOCAL>
2025-10-09 10:10:58 +0200

    Feladat is ready

---
+++ KuruczG-C#feladatok/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuruczG_C_feladatok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adja meg az összeget: ");
+            int osszeg = int.Parse(Console.ReadLine());
+            Console.Write("Adja meg hány évre szeretné lekötni: ");
+            int ev = int.Parse(Console.ReadLine());
+            Console.Write("Adja meg a kamatot: ");
+            double kamat = double.Parse(Console.ReadLine()) / 100;
+            double vegosszeg = osszeg * kamat * ev + (osszeg * kamat);
+
+            Console.WriteLine($"A végösszeg {vegosszeg} Ft");
+
+        }
+    }
+}
